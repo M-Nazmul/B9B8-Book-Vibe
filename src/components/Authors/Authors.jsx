35f4e939724f9631f612c3bc43b6eaa385b78c42
M-Nazmul <najmulni839@gmail.com>
2024-03-28 23:55:34 +0600

    added author component

--- src/components/Authors/Authors.jsx
+++ src/components/Authors/Authors.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import AuthorDetails from "../AuthorDetails/AuthorDetails";
+
 const Authors = () => {
+    const [authors, setAuthors] = useState([]);
+    useEffect(() => {
+        fetch('../../../public/books.json')
+        .then(res => res.json())
+        .then(data => setAuthors(data))
+    },[])
     return (
-        <div>
-            <h2>Author components</h2>
+        <div className="text-center">
+            <h2 className="text-xl md:text-3xl font-bold my-6 bg-slate-100 p-3 rounded-md">Authors Details</h2>
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 my-12">
+                {
+                    authors.map((authorDet, idx) => <AuthorDetails key={idx} authorDet={authorDet}></AuthorDetails>)
+                }
+            </div>
         </div>
     );
 };
